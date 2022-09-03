256404001dd72dcd815f0b277376ef85048801e0
alib4 <alibcr80547@gmail.com>
2022-09-03 17:54:38 +0430

    route dashboard page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,8 +2,18 @@ const { Router } = require('express');
 
 const router = new Router();
 
+// @Desc   Dashboard
+// @Route  GET /dashboard
+router.get('/',(req,res) => {
+    res.render("dashboard", 
+    {pageTitle:"بخش مدیریت | داشبورد", 
+    path:"/dashboard",
+    layout:"./layouts/dashLayout",
+});
+});
+
 // @Desc   Login Page
-// @Router GET /dashboard/login
+// @Route  GET /dashboard/login
 router.get("/login",(rew,res) => {
     res.render("login",{
         pageTitle: " ورورد به بخش مدیریت ",
