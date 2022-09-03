ffea81b3e7f3599a517d6e4b0616cae9e0f9fa93
alib4 <alibcr80547@gmail.com>
2022-09-03 21:36:54 +0430

    create user model in database

---
+++ model/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type: String,
+        required: true,
+        trim: true
+    },
+     email:{
+        type: String,
+        required: true,
+        unique: true
+     },
+     password:{
+        type: String,
+        required: true,
+        minlength: 4,
+        maxlength: 255
+     },
+     createdAt:{
+        type: Date,
+        default: Date.now
+     }
+});
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = User;
