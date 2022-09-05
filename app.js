53d80820ac56321f1c5b516b1e7e7f619303f5c3
alib4 <alibcr80547@gmail.com>
2022-09-05 23:34:03 +0430

    update app.js

--- app.js
+++ app.js
@@ -1,45 +1,46 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const dotEnv = require('dotenv');
-const morgan = require('morgan');
-const expresslayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const dotEnv = require("dotenv");
+const morgan = require("morgan");
 
+const connectDB = require("./config/db");
 
-const connectDb = require('./config/db');
+//* Load Config
+dotEnv.config({ path: "./config/config.env" });
 
-
-//* Load Config 
-dotEnv.config({path:"./config/config.env"});
+//* Database connection
+connectDB();
 
 const app = express();
 
-//* Logging 
-if(process.env.NODE_ENV === "development"){
+//* Logging
+if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
-};
+}
 
+//* View Engine
+app.use(expressLayout);
+app.set("view engine", "ejs");
+app.set("layout", "./layouts/mainLayout");
+app.set("views", "views");
 
-//? View Engine (Ejs)
-app.use(expresslayouts);
-app.set("view engine","ejs");
-app.set("layout","./layouts/mainLayout");
-app.set("views","views");
+//* BodyPaser
+app.use(express.urlencoded({ extended: false }));
 
-//! BodyParser
-app.use(express.urlencoded({extended: false}));
-
-
-//* Database Connection
-connectDb();
-
-//! Statics Folder
-app.use(express.static(path.join(__dirname,"./public")));
+//* Static Folder
+app.use(express.static(path.join(__dirname, "public")));
 
 //* Routes
-app.use("/",require('./routes/blog'));
-app.use("/users",require('./routes/users'));
-app.use("/dashboard",require('./routes/dashboard'));
+app.use("/", require("./routes/blog"));
+app.use("/users", require("./routes/users"));
+app.use("/dashboard", require("./routes/dashboard"));
 
-const PORT = process.env.PORT || 3000 ;
-app.listen(PORT,() => console.log(`server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
