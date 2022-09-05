9eaa64adc5acddd7a76ff6ae0158369059aeee9e
alib4 <alibcr80547@gmail.com>
2022-09-05 23:36:47 +0430

    update db file

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDb = async () => {
+const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser : true,
@@ -15,4 +15,4 @@ const connectDb = async () => {
     };
 };
 
-module.exports = connectDb;
+module.exports = connectDB;
