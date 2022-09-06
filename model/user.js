312203f2976bb864f6a14bf0eb9dac89e30794f9
alib4 <alibcr80547@gmail.com>
2022-09-06 21:29:03 +0430

    validation with yup package for model

--- model/user.js
+++ model/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const Yup = require('yup');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     fullname: {
         type: String,
         required: true,
@@ -23,6 +24,28 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-const User = mongoose.model("User", UserSchema);
+//? Yup
+const schema = Yup.object().shape({
+    fullname: Yup.string()
+        .required("نام و نام خانوادگی الزامی میباشد")
+        .min(4," نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد")
+        .max(255,"نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد"),
+    email: Yup.string()
+        .email("ایمیل معتبر نمیباشد")
+        .required("ایمیل الزامی میباشد"),
+    password: Yup.string()
+        .required("کلمه عبور الزامی میباشد")
+        .min(4,"کلمه عبور نباید کمتر از 4 کاراکتر باشد")
+        .max(255,"کلمه عبور نباید بیشتر از 255 کاراکتر باشد"),
+    confirmPassword: Yup.string()
+        .required(" تکرار کلمه عبور الزامی میباشد")
+        .oneOf([Yup.ref("password"),null])
+});
+
+userSchema.static.userValidation = function( body ) {
+    return schema.validate( body, { abortEarly: false });
+};
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
