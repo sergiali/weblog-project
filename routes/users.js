b06a75262b02bafba31a4f95c92527af8536d68e
alib4 <alibcr80547@gmail.com>
2022-09-07 21:56:57 +0430

    set handleLogin with controller

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,10 @@ const router = new Router();
 //  @route  GET /users/login
 router.get("/login", userController.login);
 
+//  @desc   Login Handle
+//  @route  POST /users/login
+router.post("/login", userController.handleLogin);
+
 //  @desc   Register Page
 //  @route  GET /users/register
 router.get("/register",userController.register);
